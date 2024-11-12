dc87953f709b79117b566adb286a8444d3a90997
srazojr <srazojr@gmail.com>
2024-11-12 02:52:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ const db = mongoose.connection
 db.on('error', (e) => { console.log("mongodb error: " + e) })
 db.on('once', (e) => { console.log("mongodb is open") })
 
-app.listen(3000)
+
 console.log("Server Started!")
 const subscribersRouter = require("./routes/subscribers")
 app.use("/subscribers", subscribersRouter)
 
 app.use(express.json())
+
+app.listen(3000)
+
